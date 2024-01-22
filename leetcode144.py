64aae053cc1bebc6a27ecd69a87c64ff12849283
Collin Funk <collin.funk1@gmail.com>
2024-01-22 12:53:48 -0800

    Updates

---
+++ leetcode144.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+
+from typing import List, Optional
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def preorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        result_list = []
+        self.traverse(root, result_list)
+        return result_list
+
+    def traverse(self, root: Optional[TreeNode], result_list: List[int]) -> None:
+        if not root:
+            return result_list
+        else:
+            result_list.append(root.val)
+            self.traverse(root.left, result_list)
+            self.traverse(root.right, result_list)
+
+
+def main() -> None:
+    pass
+
+
+if __name__ == "__main__":
+    main()
---
+++ leetcode83.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+
+from typing import Optional
+
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+class Solution:
+    def deleteDuplicates(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        if not head or not head.next:
+            return head
+
+        current_node = head
+        while current_node:
+            if not current_node.next:
+                break
+            elif current_node.val == current_node.next.val:
+                current_node.next = current_node.next.next
+                continue
+            current_node = current_node.next
+
+        return head
+
+
+def main() -> None:
+    pass
+
+
+if __name__ == "__main__":
+    main()
