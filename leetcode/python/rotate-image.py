ef66f7eaeafff6ab75432efd62169f8821496d92
Collin Funk <collin.funk1@gmail.com>
2024-02-13 17:38:33 -0800

    Added Leetcode Python problem 48.

---
+++ leetcode/python/rotate-image.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+""" Leetcode problem 48: Rotate Image. """
+
+from typing import List
+
+class Solution:
+    def rotate(self, matrix: List[List[int]]) -> None:
+        matrix[:] = zip(*matrix[::-1])
+
+
+def main() -> None:
+    pass
+
+
+if __name__ == "__main__":
+    main()
